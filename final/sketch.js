d13253de95daa07febb15cc7e1099adb6bc0d8c9
Sarah <sarahstanislevic@gmail.com>
2022-12-27 00:03:58 -0500

    maps all points

--- final/sketch.js
+++ final/sketch.js
@@ -140,9 +140,21 @@ function initMap() {
         ['Seaport District, Manhattan', 40.70602396324339, -74.00191721690764],
         ['Rockefeller Center', 40.759292814794996, -73.97880234603359],
         ['Bronx Zoo Holiday Light Show, The Bronx, NY', 40.851211673759884, -73.87708403068848],
-        ['Hudson Yards, New York, NY', 40.75452297210832, -74.00162995448743],
-        ['Brooklyn Botanic Garden, Brooklyn, NY', 40.66844959612454, -73.96464126137813],
-
+        ['Bryant Park Winter Village', 40.75408726750916, -73.98367083068877],
+        ['Radio City Music Hall, Midtown', 40.759227801528134, -73.97880234603359],
+        ['Washington Square Park, Greenwich Village', 40.730786238410325, -73.99737491534452],
+        ['Chelsea Market,', 40.743122408505805, -74.00605806931092],
+        ['SUMMIT One Vanderbilt', 40.75341419302138, -73.97870318465547],
+        ['Pulitzer Fountain & The Plaza Hotel, Midtown', 40.76467348290213, -73.97366621534454],
+        ['Fifth Avenue, Midtown', 40.75350933628704, -73.98104438853086],
+        ['Zuccotti Park', 40.70988298745567, -74.011262],
+        ['Brookfield Place', 40.712879407367424, -74.01527387819604],
+        ['Winter Lantern Festival, Queens, NY', 40.74812874470595, -73.7221346460336],
+        ['73rd Street, UES', 40.77002783854908, -73.95874563287431],
+        ['Santaland, Union Square', 40.751578743164174, -73.98909610000001],
+        ['Hudson Yards, New York, NY', 40.754449826930674, -74.00237024418067],
+        ['Brooklyn Botanic Garden, Brooklyn, NY', 40.66880763550689, -73.96446960218861],
+        ['The Shops at Columbus Circle', 40.768857346388785, -73.98312745800453],
 
         
     ];
@@ -152,7 +164,7 @@ function initMap() {
         ['<div class="info_content">' +
         '<h2>Harvest Moon Farm & Orchard</h2>' +
         '<h3>130 Hardscrabble Rd, North Salem, NY 10560</h3>' +
-        '<p>Year-round farm with a produce stand & a bakery, plus fall apple picking & holiday light shows.</p>' + 
+        '<p>This gorgeous illuminated walking trail located about an hour outside of NYC features over 750,000 LED lights that are perfect for your next Instagram post! There are different areas scattered around the Harvest Moon Farm & Orchard including Sparkling light, Into the Wonderland, Magical Trees, A great Celebration, Treats and Sweet, and Beyond the Forest!</p>' + 
         '</div>'],
 
         ['<div class="info_content">' +
@@ -195,24 +207,99 @@ function initMap() {
         '<h2>Bronx Zoo Holiday Light Show</h2>' +
         '<h3>River Entrance, 2300 Southern Blvd, The Bronx, NY 10460</h3>' +
         '<p>Covering several acres of the zoo, explore a glowing forest at this year’s Bronx Zoo Lights. See larger-than-life toads, frogs, snails, sunbirds, and more with 21 beaming lanterns.</p>' +
-        '</div>']
-
+        '</div>'],
 
         ['<div class="info_content">' +
-        '<h2>Hudson Yards Holiday Display</h2>' +
-        '<h3>30 Hudson Yards, New York, NY 10001</h3>' +
-        '<p>This year, Hudson Yards brought back their stunning holiday installation called “Shine Bright.” The neighborhood is decorated in more than two million lights! Prepare to be dazzled by the shopping center’s interior decked out in shimmering string lights. Step outside to see the vessel gleaming gold alongside other holiday trees and the beautiful hot air balloon display!</p>' +
+        '<h2>Bryant Park Winter Village & Holiday Market</h2>' +
+        '<h3>42nd Street and, 6th Ave, New York, NY 10018</h3>' +
+        '<p>It’s no secret that we love the Bryant Park Holiday Market and Winter Village, and they have a bunch of lights that makes it even more festive. Plus, what could be better than looking at some holiday lights while shopping for presents, or even ice skating for free? There’s so much to do!</p>' +
         '</div>'],
         
+         ['<div class="info_content">' +
+        '<h2>Radio City Music Hall</h2>' +
+        '<h3>45 Rockefeller Plaza, New York, NY 10111</h3>' +
+        '<p>Radio City Music Hall is an iconic NYC venue, especially during the holiday season when The Rockettes famously take it over for a month! Now that they’re back on show, we suggest checking them out, along with their gorgeous Christmas tree that sits on the roof of the building.</p>' +
+        '</div>'],
+
+          ['<div class="info_content">' +
+        '<h2>Washington Square Park Tree</h2>' +
+        '<h3>Washington Square, New York, NY 10012</h3>' +
+        '<p>Being situated right under the Washington Square Arch, the angle of this incredible illumination is one of our favorites in all of NYC!</p>' + 
+        '</div>'],
+
+           ['<div class="info_content">' +
+        '<h2>Chelsea Market</h2>' +
+        '<h3>75 9th Ave, New York, NY 10011</h3>' +
+        '<p>Walk under multi-colored mylar fringe and ornaments when you visit Chelsea Market. You’ll find dazzling lights along the exterior of the market and decorative presents, lit up Christmas trees, and shiny ornaments on the inside.</p>' +
+        '</div>'],
+
+          ['<div class="info_content">' +
+        '<h2>SUMMIT One Vanderbilt Light and Sound Show</h2>' +
+        '<h3>45 E 42nd St, New York, NY 10017</h3>' +
+        '<p>Experience a gasp-worthy light and sound show “AIR at Night” at the world’s most immersive observatory, SUMMIT One Vanderbilt.</p>' +
+        '</div>'],
+
+         ['<div class="info_content">' +
+        '<h2>Pulitzer Fountain & The Plaza Hotel</h2>' +
+        '<h3>764 Central Park S, New York, NY 10019</h3>' +
+        '<p>Right across from The Plaza Hotel and east entrance to Central Park, see 32 animal sculptures leap, jump, and skate around the Pulitzer Fountain that’s been decked out in 5,000 feet of lights! The arctic display is made up of 24 floating icebergs and a skating rink! </p>' +
+        '</div>'],
+
+         ['<div class="info_content">' +
+        '<h2>Illuminated Toy Installations on 5th Ave</h2>' +
+        '<h3>5th Ave between 48th St – 57th St, New York, NY</h3>' +
+        '<p>The beloved illuminated toy installations are back on Fifth Ave again this year along with an initiative called “Fifth Avenue for All.” Visitors are able to see their favorite childhood toys like teddy bears and jack-in-the-boxes reincarnated in glistening lights! The lights are up now, but are able to being explored even more on select Sundays, when the whole street will be blocked off.</p>' + 
+        '</div>'],
+
+        ['<div class="info_content">' +
+        '<h2>Tree Light Display at Zuccotti Park</h2>' +
+        '<h3>New York, NY 10006</h3>' +
+        '<p>Admire thousands of lights on every branch of every tree in Zuccotti Park for a wowing display of holiday cheer.</p>' +
+        '</div>'],
+
+        ['<div class="info_content">' +
+        '<h2>Brookfield Place Luminaries Exhibit & Iceskating</h2>' +
+        '<h3>230 Vesey St, New York, NY 10281</h3>' +
+        '<p>The Luminaries exhibit has returned to Brookfield Place and it’s a colorful wonderland. Located in the Winter Garden, head over to make a holiday wish. With every wish, $1 will be donated to City Harvest.</p>' +
+        '</div>'],
+
+        ['<div class="info_content">' +
+        '<h2>Winter Lantern Festival at Queens County Farm Museum</h2>' +
+        '<h3>73-50 Little Neck Pkwy, Queens, NY 11004</h3>' +
+        '<p>Created as a way for visitors to experience and learn about the culture and craftsmanship of traditional Chinese lantern festivals, three incredible Winter Lantern Festival events are available this season across Queens Staten Island, and Long Island. Enter into a whimsical world of light and be awed by thousands of handmade glowing lantern figures.</p>' +
+        '</div>'],
+
+        ['<div class="info_content">' +
+        '<h2>Lights on 73rd Street</h2>' +
+        '<h3>New York, NY 10021</h3>' +
+        '<p>This street on the Upper East Side is lined with tons of twinkling trees. Take a lovely stroll through the neighborhood and try to find more gorgeously festive decorations throughout!</p>' +
+        '</div>'],
+
+         ['<div class="info_content">' +
+        '<h2>Santaland at Macys Herald Square</h2>' +
+        '<h3>151 W 34th St., New York, NY 10001</h3>' +
+        '<p>The famed Macys store mounts a 13,000-square-foot North Pole Christmas village complete with live elves, an enchanted forest featuring animatronic toys, and a train display. At the end, of course, you get to visit with the one true Santa.</p>' +
+        '</div>'],
+
+          ['<div class="info_content">' +
+        '<h2>Light Display at Hidson Yards</h2>' +
+        '<h3>New York, NY 10001</h3>' +
+        '<p>This year, Hudson Yards brought back their stunning holiday installation called “Shine Bright.” The neighborhood is decorated in more than two million lights! Prepare to be dazzled by the shopping center’s interior decked out in shimmering string lights. Step outside to see the vessel gleaming gold alongside other holiday trees and the beautiful hot air balloon display!</p>'+
+       '</div>'],
+
         ['<div class="info_content">' +
         '<h2>Brooklyn Botanic Garden Light Show</h2>' +
         '<h3>990 Washington Ave, Brooklyn, NY 11225</h3>' +
         '<p>Roam under one million sparkling lights at Brooklyn Botanic Garden’s astonishing holiday trail. Walk along the garden’s landscapes to a curated soundtrack and experience incredible light sculptures, & vibrant effects on the BBG’s landscapes that will illuminate the many trees and water features.</p>' +
-        '</div>']
+        '</div>'],
+
+        ['<div class="info_content">' +
+        '<h2>Lights at The Shops at Columbus Circle</h2>' +
+        '<h3>10 Columbus Circle, New York, NY 10019</h3>' +
+        '<p>The 14-foot stars have reappeared at The Shops at Columbus Circle and they even dance! Starting at 5 p.m. daily the stars light up in synchronization with holiday music during the Holiday Under the Stars Show at every half hour until 11:00 p.m. These stars will illuminate until January 3rd.</p>' +
+        '</div>'],
 
 
-
-       
     ];
         
     // Add multiple markers to map
